6924d2e68f65de125f6088c34335a0968f417380
ypuchong <katimere15@gmail.com>
2022-03-10 16:53:13 +0900

    03102

---
+++ 0310_codingtest/python_codingtest10.py
@@ -0,0 +1,28 @@
+"""
+문제 설명
+정수를 저장한 배열, arr 에서 가장 작은 수를 제거한 배열을 리턴하는 함수, solution을 완성해주세요. 단, 리턴하려는 배열이 빈 배열인 경우엔 배열에 -1을 채워 리턴하세요. 예를들어 arr이 [4,3,2,1]인 경우는 [4,3,2]를 리턴 하고, [10]면 [-1]을 리턴 합니다.
+
+제한 조건
+arr은 길이 1 이상인 배열입니다.
+인덱스 i, j에 대해 i ≠ j이면 arr[i] ≠ arr[j] 입니다.
+입출력 예
+arr	return
+[4,3,2,1]	[4,3,2]
+[10]	[-1]
+"""
+#내가 풀이 한것 
+def solution(arr):
+    if 1>=len(arr):
+        arr2 = [-1]
+        return arr2
+    min_num = min(arr)
+    arr.remove(min_num)
+    return(arr)
+#다른분들 풀이
+def rm_small(mylist):
+    return [i for i in mylist if i > min(mylist)]
+
+# 아래는 테스트로 출력해 보기 위한 코드입니다.
+my_list = [4,3,2,1]
+print("결과 {} ".format(rm_small(my_list)))
+
